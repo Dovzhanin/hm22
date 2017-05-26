244ec94b0bf8683d7ea2b756b6b24c535074ddd1
Veronika <dovzhaninveronika@gmail.com>
2017-05-26 18:25:37 +0300

    3

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-
-
 var arr = [1,7,0,8,6];
 console.log(arr);
 arr.length = 0;
@@ -17,20 +15,22 @@ console.log(arr);
 
 
 var  fruits = ["Banana", "Orange", "Apple", "Mango"];
-var howmany = (fruits.length);
-for(var i = 0; i <= howmany; i++){
+var howmany = fruits.length);
+for(var i = 0; i < howmany; i++) {
   console.log(fruits[i]);
 }
 
 
-var start = [100];
-var result = [100];
+var start = 100;
+var pushed = 0;
+var result = [];
 for(; start >= 0; start--) {
-  result.push(start);
+ pushed = result.push(start);
 }
+ console.log(result);
 
 
 
 var test = ['Banana','Orange','Apple', 'Mango'];
-test.splice(-1, 1, 'Raspberry')
+test.splice(test.length -2, 1, 'Raspberry')
 console.log(test);
